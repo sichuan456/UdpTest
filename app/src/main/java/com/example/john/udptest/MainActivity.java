ce62e5cdad9e13a22774c2fc627ca384a329bc29
leo123 <76928282@163.com>
2016-07-16 15:40:15 +0800

    ss

--- app/src/main/java/com/example/john/udptest/MainActivity.java
+++ app/src/main/java/com/example/john/udptest/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("ssoutouooooo999999999999","sssoooooo99999999999999");
+        Log.e("s1111111","1111111111");
 
         execPostMethod();
 
